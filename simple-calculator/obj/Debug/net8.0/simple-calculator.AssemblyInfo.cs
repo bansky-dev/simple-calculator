4e272b0825096b9fc1bd234591ff66f094bf77f2
Kociarz <dbednorz@tuta.io>
2024-10-05 17:24:30 +0200

    Fixed README file

--- simple-calculator/obj/Debug/net8.0/simple-calculator.AssemblyInfo.cs
+++ simple-calculator/obj/Debug/net8.0/simple-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simple-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb5f911efc7a59543f366a8281e949a2c5391012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e999cfa473b366f2e44d71f3e0406e219e92929")]
 [assembly: System.Reflection.AssemblyProductAttribute("simple-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simple-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

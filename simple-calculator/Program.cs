bff04df6e064ed4337d0c2f7593e6a677312e9e2
Kociarz <dbednorz@tuta.io>
2024-10-05 17:33:19 +0200

    Code improvemnt

--- simple-calculator/Program.cs
+++ simple-calculator/Program.cs
@@ -6,20 +6,20 @@ class Program
 
     static void Main(string[] args)
     {
-        System.Console.WriteLine("=====SIMPLE-CALCULATOR=====");
-        System.Console.Write("Podaj pierwsza liczbe: ");
+        Console.WriteLine("=====SIMPLE-CALCULATOR=====");
+        Console.Write("Podaj pierwsza liczbe: ");
 
         float firstNumber = int.Parse(Console.ReadLine());
 
-        System.Console.Write("Podaj druga liczbe: ");
+        Console.Write("Podaj druga liczbe: ");
 
         float secondNumber = int.Parse(Console.ReadLine());
 
-        System.Console.WriteLine("Co chcesz zrobic?");
-        System.Console.WriteLine("[1] Dodawanie");
-        System.Console.WriteLine("[2] Odejmowanie");
-        System.Console.WriteLine("[3] Mnożenie");
-        System.Console.WriteLine("[4] Dzielenie");
+        Console.WriteLine("Co chcesz zrobic?");
+        Console.WriteLine("[1] Dodawanie");
+        Console.WriteLine("[2] Odejmowanie");
+        Console.WriteLine("[3] Mnożenie");
+        Console.WriteLine("[4] Dzielenie");
         Console.Write("Wybor: ");
         int operatorChoice = int.Parse(Console.ReadLine());
 
